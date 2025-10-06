a7af1df67532c4fe35c6a3cff2e8362a2f983840
samar2341 <samardotexe@gmail.com>
2025-10-06 21:43:13 +0530

    Update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -48,6 +48,7 @@ export const Hero = () => {
             className="border-primary text-primary hover:bg-primary hover:text-background font-semibold"
             asChild
           >
+            
             <a href="../assets/samarcv.pdf" target="_blank" rel="noopener noreferrer">
               <FileText className="mr-2 h-5 w-5" />
               Resume
